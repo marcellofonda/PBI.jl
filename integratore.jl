970fa173de8f8b77207a532d253848351c93983d
marcellofonda <marcellofonda@gmail.com>
2023-04-01 21:55:19 +0200

    Better performance on integratore.jl
    - remove quicksort, since Julia's native sort! is more efficient
    - better commenting
    - improve performance by approx 100x: instead of iterating over pixels, and over faces for each pixel, it now iterates on faces, and on pixel within its bounding box for each pixel
    - Print the percentage of faces covered instead of the current pixel
    - Improved parallelization: now only truly independent cycles are parallelized
    - Remove useless commented debug code
    - Now check if triangle is degenerate (orthogonal to yz plane) and use the barycenter twice in that case (thickness unaffected by degenerate triangles)
    - Comment some unused debug code

--- integratore.jl
+++ integratore.jl
@@ -50,31 +50,20 @@ function load_obj(filename::AbstractString)
     return vertices, faces
 end
 
-# function quicksort(arr)
-#     if length(arr) <= 1
-#         return arr
-#     end
-#
-#     pivot = arr[rand(1:end)]
-#
-#     left = collect(filter(x -> x < pivot, arr))
-#     middle = collect(filter(x -> x == pivot, arr))
-#     right = collect(filter(x -> x > pivot, arr))
-#
-#     return vcat(quicksort(left), middle, quicksort(right))
-# end
-
-
 function triangle_contains_point(tri1,tri2,tri3, y::Float64, z::Float64)
     # Get the x, y, and z coordinates of the triangle vertices
     x1, y1, z1 = tri1
     x2, y2, z2 = tri2
     x3, y3, z3 = tri3
 
+    # Get the 3 vectors representing, whith respect to the projection of v1 on the
+    # yz plane, the other vertices and [y,z]
     v0 = [y2 - y1, z2 - z1]
     v1 = [y3 - y1, z3 - z1]
     v2 = [y - y1, z - z1]
 
+    # Do the maths of using barycentric coordinates. Idk what this means, but
+    # it seems like ChatGPT knows what they're doing.
     denom = v0[1]*v1[2] - v0[2]*v1[1]
     u = (v2[1]*v1[2] - v2[2]*v1[1]) / denom
     v = (v0[1]*v2[2] - v0[2]*v2[1]) / denom
@@ -90,10 +79,10 @@ function project_mesh(vertices, faces, n)
     # Initialize output matrix
     matrix = [[[] for j in 1:n] for i in 1:n]
 
-    #println(matrix)
-    # Calculate center of mass
-    #com = sum(vertices) / size(vertices, 1)
     nverts = size(vertices, 1)
+
+    # Find the bounding box of the projection of the mesh on the yz plane
+    # i.e. minimum and maximum values of y and z.
     min_y = minimum([ vertices[i][2] for i in 1:nverts ])
     max_y = maximum([ vertices[i][2] for i in 1:nverts ])
     min_z = minimum([ vertices[i][3] for i in 1:nverts ])
@@ -103,85 +92,128 @@ function project_mesh(vertices, faces, n)
         vertices[i] -= [0, min_y, min_z]
     end
 
-    scaling = maximum([max_y-min_y max_z-min_z])
+    # Find the normalization constant to fit the mesh into the [0,1]x[0,1] square
+    # i.e. the maximum side length of the bounding box. This will be used to
+    # correctly spread the pixels throughout the 3D space.
+    scaling = maximum([max_y-min_y, max_z-min_z])
 
-    # Project vertices onto yz plane and scale to matrix size
-    #projected_vertices = round.(Int, n * (vertices[:, 2:3] .- com[2:3]))
-
+    println("Projecting matrix ...")
     println()
-    # Iterate over rows of matrix
-    for i = 1:n
-        # Compute the corresponding y value CAUTION!!! probably wrong
-        y = (i - 0.5) * scaling / n
-        # Iterate over columns of matrix
-        for j = 1:n
-            # Compute the corresponding z value CAUTION!!! probably wrong
-            z = (j - 0.5) * scaling / n
 
-            print("currently at [$i, $j]...", "   ")
-            print("\r")
+    n_faces = length(faces)
+    face_counter = 0
+    for face in faces
 
-            # Where the x coordinates of the surface of the mesh
-            # will be saved.
-            extrema = []
+        # Print the progress as a percentage of faces mapped
+        percentage = div(face_counter*100,n_faces)
+        face_counter += 1
+        print("$percentage%", "   ")
+        print("\r")
 
-            # Iterate over faces
-            Threads.@threads for face in faces
-                # Calculate plane of face
-                v1 = vertices[face[1]]
-                v2 = vertices[face[2]]
-                v3 = vertices[face[3]]
+        # Calculate plane of face
+        v1 = vertices[face[1]]
+        v2 = vertices[face[2]]
+        v3 = vertices[face[3]]
 
+        normal = cross(v2 - v1, v3 - v1)
+        d = -dot(normal, v1)
+
+        # If the normal vector to the triangle has no x component, then the projection
+        # on the yz plane will be degenerate
+        is_degenerate = (normal[1] == 0)
+
+        # if (is_degenerate)
+        #     println("degenere")
+        # end
+
+        # Find the bounding box of the triangle
+        max_y_tri = maximum( [ v1[2], v2[2], v3[2] ] )
+        min_y_tri = minimum( [ v1[2], v2[2], v3[2] ] )
+        max_z_tri = maximum( [ v1[3], v2[3], v3[3] ] )
+        min_z_tri = minimum( [ v1[3], v2[3], v3[3] ] )
+
+        # Iterate over rows of matrix
+        Threads.@threads for i = 1:n
+            # Compute the corresponding y value
+            y = (i - 0.5) * scaling / n
+
+            # Check if we are within the bounding box
+            if (y < min_y_tri)
+                continue
+            elseif (y > max_y_tri)
+                break
+            end
+
+            # Iterate over columns of matrix
+            for j = 1:n
+                # Compute the corresponding z value
+                z = (j - 0.5) * scaling / n
+
+                # Check if we are within the bounding box
+                if (z < min_z_tri)
+                    continue
+                elseif (z > max_z_tri)
+                    break
+                end
+
+                # Where the x coordinates of the surface of the mesh
+                # will be saved.
+                extrema = matrix[i][j]
 
                 # Check if point is inside the face. I currently don't
                 # trust this method, but it's all I have so far. Might
-                # write another if it doesn't work
+                # write another if it doesn't work.
+                # But somehow, it works... Must study some more maths...
                 if triangle_contains_point(v1,v2,v3, y, z)
-
-                    normal = cross(v2 - v1, v3 - v1)
-                    d = -dot(normal, v1)
-                    #plane = [normal; -d]
 
                     # Calculate where the point with
                     # given yz coordinates is on the mesh
-                    # through the equation of the plane
-                    x = (-dot(normal,[0,y,z]) - d) / normal[1]
+                    # through the equation of the plane.
+                    # If the triangle is projection-degenerate,
+                    # save its center of mass
+                    if (is_degenerate)
+                        x = sum([v1, v2, v3])[1]/3
+                    else
+                        x = (-dot(normal,[0,y,z]) - d) / normal[1]
+                    end
 
                     #If it's not already there...
                     if !(any(isapprox.(extrema, x, rtol=0.00001)))
+
                         # If it is contained, then save it.
                         push!(extrema, x)
+
+                        # If the projection of the triangle is degenerate, then save the projection twice
+                        # This way, the thickness will be unaffected by this point.
+                        if (is_degenerate)
+                            push!(extrema, x)
+                        end
                     end
                 end
             end
             # Sort the results, which should be in even number
             # I should add a check to see if one point is tangent.
             # In that case, calculating integrals would be a pain in the ass...
-            sort!(extrema)
 
-            # Save all the x coordinates of the points
-            # in the projection matrix. We can use them later to Compute
-            # integrals or other stuff.
-            matrix[i][j]=extrema
         end
     end
 
     println("Done projecting matrix!           ")
-    return hcat([s for s in matrix]...)
+
+    matrix = hcat([s for s in matrix]...)
+    sort!.(matrix)
+
+    return matrix
 end
 
-
-projection_matrix = project_mesh(load_obj("$filename.obj")..., resolution)
-
-
-contatore = 0
+#contatore = 0
 function thickness(vector)
     thick = 0
-    leng=length(vector)
-    if(leng % 2 != 0)
-        global contatore
+    leng = length(vector)
+    if (leng % 2 != 0)
+        #global contatore
         println("Oppa, qua ne ho trovati $leng : $vector")
-        contatore += 1
+        #contatore += 1
     end
     for i in 1:length(vector)
         thick += (-1)^i * vector[i]
@@ -189,12 +221,11 @@ function thickness(vector)
     return thick
 end
 
+projection_matrix = project_mesh(load_obj("obj\\$filename.obj")..., resolution)
+
 image = Matrix{Float64}(thickness.(projection_matrix))
 
-
-max = maximum(image)
 clamper = scaleminmax(0,max)
 image =  map(clamper, image)
-
 
 save("$filename.png", image)
